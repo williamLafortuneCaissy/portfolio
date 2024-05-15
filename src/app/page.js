212669bb62573b53c92ec606fd3f87637ff5d6c2
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2024-05-15 13:52:06 -0400

    added benefits

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Hero from "@/components/hero/Hero";
 import styles from "./page.module.css";
+import Benefits from "@/components/benefits/Benefits";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
-      Hello World
+      <Benefits />
     </main>
   );
 }
