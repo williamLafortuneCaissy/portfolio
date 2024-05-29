ae382845200d67cb8b1c5d61568cf469065af15d
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2024-05-29 13:32:29 -0400

    updated updated image to a graphic

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Frameworks from "@/components/frameworks/Frameworks";
 import Design from "@/components/design/Design";
 import Contact from "@/components/contact/Contact";
 import GridSpot from "@/components/gridSpot/GridSpot";
-import Image from "next/image";
+import PlanetGraphic from "@/components/planetGraphic/PlanetGraphic";
 
 export default function Home() {
   return (
@@ -18,7 +18,7 @@ export default function Home() {
           </div>
           <Frameworks />
           <Design />
-          <Image className={`${styles.heroImg} ${styles.inverted}`} src="./hero-bg.png" alt="" width={1440} height={233} />
+          <PlanetGraphic inverted />
       </main>
       <Contact />
     </>
