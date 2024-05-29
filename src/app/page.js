f5163c2ec08d65650d989e41a95a148615471256
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2024-05-29 16:08:40 -0400

    fixed footer links (hint: z-index -1, guess why <.<)

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,10 @@ import Frameworks from "@/components/frameworks/Frameworks";
 import Design from "@/components/design/Design";
 import Contact from "@/components/contact/Contact";
 import GridSpot from "@/components/gridSpot/GridSpot";
-import PlanetGraphic from "@/components/planetGraphic/PlanetGraphic";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.body}>
       <main className={styles.main}>
           <Hero />
           <Benefits />
@@ -20,6 +19,6 @@ export default function Home() {
           <Design />
       </main>
       <Contact />
-    </>
+    </div>
   );
 }
